95208ea33793f09a15dd9bc19584258dc5408bf0
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-01-19 11:24:00 +0700

    Add: add index.ts to export all admin components

---
+++ client/app/pages/admin/components/index.ts
@@ -0,0 +1,17 @@
+import EditTableModal from "./EditTableModal";
+import EditUserModal from "./EditUserModal";
+import MenuManagement from "./MenuManagement";
+import TableManagement from "./TableManagement";
+import StaffManagement from "./StaffManagement";
+import StatsGrid from "./StatsGrid";
+import TableQR from "./TableQr";
+
+export {
+    EditTableModal,
+    EditUserModal,
+    MenuManagement,
+    TableManagement,
+    StaffManagement,
+    StatsGrid,
+    TableQR
+}
