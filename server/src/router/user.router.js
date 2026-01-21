77bcbb7d0cc4a5be8161ecad8295ad86a5a63568
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-01-21 17:40:27 +0700

    Add: use authorized roles middleware for user route

--- server/src/router/user.router.js
+++ server/src/router/user.router.js
@@ -1,18 +1,39 @@
 import express from "express";
 import * as UserController from "../controller/user.controller.js";
+import { validateToken } from "../middleware/jwt.middleware.js";
+import authorizedRoles from "../middleware/role.middleware.js";
 
 const router = express.Router();
 
 router.post(
   "/create-tenant",
+  validateToken,
+  authorizedRoles("Developer"),
   UserController.createTenantController
 );
+router.post("/create-staff", 
+  validateToken,
+  authorizedRoles("ADMIN", "Developer"), 
+  UserController.createStaffController
+);
+router.post("/delete-staff/:id", 
+  validateToken,
+  authorizedRoles("ADMIN", "Developer"), 
+  UserController.deleteStaffController
+);
+router.put("/update-staff/:id", 
+  validateToken, 
+  authorizedRoles("ADMIN", "Developer"), 
+  UserController.updateStaffController
+);
 
-router.post("/create-staff", UserController.createStaffController);
-router.post("/delete-staff/:id", UserController.deleteStaffController);
-router.put("/update-staff/:id", UserController.updateStaffController);
-
-router.get("/cashier/:restaurantId", UserController.getCashierByRestaurantIdController);
-router.get("/kitchen/:restaurantId", UserController.getKitchenByRestaurantIdController);
+router.get("/cashier/:restaurantId", 
+  validateToken, 
+  UserController.getCashierStaffByRestaurantIdController
+);
+router.get("/kitchen/:restaurantId", 
+  validateToken, 
+  UserController.getKitchenStaffByRestaurantIdController
+);
 
 export default router;
