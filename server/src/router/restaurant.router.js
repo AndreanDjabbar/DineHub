731226638bb13cc2305342dd6b04a460985a4e4e
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-01-22 16:43:03 +0700

    Add: add validate token and authorize user roles middleware

--- server/src/router/restaurant.router.js
+++ server/src/router/restaurant.router.js
@@ -1,21 +1,68 @@
 import express from "express";
 import * as RestaurantController from "../controller/restaurant.controller.js";
+import { validateToken } from "../middleware/jwt.middleware.js";
+import authorizedRoles from "../middleware/role.middleware.js";
+validateToken
 
 const router = express.Router();
 
-router.post("/onboard", RestaurantController.onboardTenantController);
-router.get("/restaurants", RestaurantController.getAllRestaurantsController);
-router.get("/:id", RestaurantController.getRestaurantController);
-router.put("/:id", RestaurantController.updateRestaurantController);
-router.delete("/:id", RestaurantController.deleteRestaurantController);
-
+router.post(
+  "/onboard", 
+  validateToken, 
+  authorizedRoles("Developer"),
+  RestaurantController.onboardTenantController
+);
+router.get(
+  "/restaurants", 
+  validateToken, 
+  authorizedRoles("Developer"),
+  RestaurantController.getAllRestaurantsController
+);
+router.get(
+  "/:id", 
+  validateToken, 
+  authorizedRoles("Developer", "ADMIN"),
+  RestaurantController.getRestaurantController
+);
+router.put(
+  "/:id", 
+  validateToken, 
+  authorizedRoles("Developer"),
+  RestaurantController.updateRestaurantController
+);
+router.delete(
+  "/:id", 
+  validateToken, 
+  authorizedRoles("Developer"),
+  RestaurantController.deleteRestaurantController
+);
 router.get(
   "/tables/:restaurantId",
+  validateToken,
+  authorizedRoles("Developer", "ADMIN", "CASHIER"),
   RestaurantController.getTablesByRestaurantIdController
 );
-router.get("/table/:id", RestaurantController.getTableByIdController);
-router.post("/tables", RestaurantController.createTableController);
-router.delete("/tables/:id", RestaurantController.deleteTableController);
-router.put("/tables/:id", RestaurantController.updateTableController);
+router.get(
+  "/table/:id",
+  RestaurantController.getTableByIdController
+);
+router.post(
+  "/tables", 
+  validateToken, 
+  authorizedRoles("Developer", "ADMIN"),
+  RestaurantController.createTableController
+);
+router.delete(
+  "/tables/:id", 
+  validateToken, 
+  authorizedRoles("Developer", "ADMIN"),
+  RestaurantController.deleteTableController
+);
+router.put(
+  "/tables/:id", 
+  validateToken, 
+  authorizedRoles("Developer", "ADMIN"),
+  RestaurantController.updateTableController
+);
 
 export default router;
