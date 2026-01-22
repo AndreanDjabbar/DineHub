2604c151ff0d100609832547b6f3950ff03badfb
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-01-22 17:26:31 +0700

    Add: use validate schema for user route

--- server/src/router/user.router.js
+++ server/src/router/user.router.js
@@ -2,17 +2,26 @@ import express from "express";
 import * as UserController from "../controller/user.controller.js";
 import validateToken from "../middleware/jwt.middleware.js";
 import authorizedRoles from "../middleware/role.middleware.js";
+import validateSchema from "../middleware/schema.middleware.js";
+
+import { 
+  createStaffSchema ,
+  createTenantAdminSchema,
+  updateStaffSchema
+} from "../validation/user.validation.js";
 
 const router = express.Router();
 
 router.post(
   "/create-tenant",
   validateToken,
+  validateSchema(createTenantAdminSchema),
   authorizedRoles("Developer"),
   UserController.createTenantController
 );
 router.post("/create-staff", 
   validateToken,
+  validateSchema(createStaffSchema),
   authorizedRoles("ADMIN", "Developer"), 
   UserController.createStaffController
 );
@@ -23,10 +32,10 @@ router.post("/delete-staff/:id",
 );
 router.put("/update-staff/:id", 
   validateToken, 
+  validateSchema(updateStaffSchema),
   authorizedRoles("ADMIN", "Developer"), 
   UserController.updateStaffController
 );
-
 router.get("/cashier/:restaurantId", 
   validateToken, 
   UserController.getCashierStaffByRestaurantIdController
