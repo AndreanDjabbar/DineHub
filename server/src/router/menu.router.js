99f9b480d7580e82820613bc599c2e340517fff8
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-01-23 15:09:13 +0700

    Add: use schema validation for menu router

--- server/src/router/menu.router.js
+++ server/src/router/menu.router.js
@@ -1,16 +1,52 @@
 import express from "express";
 import * as MenuController from "../controller/menu.controller.js";
+import validateToken from "../middleware/jwt.middleware.js";
+import validateSchema from "../middleware/schema.middleware.js";
+import authorizedRoles from "../middleware/role.middleware.js";
+import { 
+    createMenuCategorySchema,
+    createMenuItemSchema,
+    updateMenuItemSchema
+} from "../validation/menu.validation.js";
 
 const router = express.Router();
 
-// Categories
-router.post("/categories", MenuController.createCategoryController);
-router.get("/categories/:restaurantId", MenuController.getCategoriesByRestaurantIdController);
-router.delete("/categories/:id", MenuController.deleteCategoryController);
-
-// Menu Items
-router.post("/items", MenuController.createMenuItemController);
-router.put("/items/:id", MenuController.updateMenuItemController);
-router.delete("/items/:id", MenuController.deleteMenuItemController);
+router.post(
+    "/categories", 
+    validateToken,
+    validateSchema(createMenuCategorySchema), 
+    authorizedRoles("Developer", "ADMIN"),
+    MenuController.createCategoryController
+);
+router.get(
+    "/categories/:restaurantId", 
+    MenuController.getCategoriesByRestaurantIdController
+);
+router.delete(
+    "/categories/:id", 
+    validateToken,
+    authorizedRoles("Developer", "ADMIN"),
+    MenuController.deleteCategoryController
+);
+router.post(
+    "/items",
+    validateToken, 
+    validateSchema(createMenuItemSchema), 
+    authorizedRoles("Developer", "ADMIN"),
+    MenuController.createMenuItemController
+);
+router.put(
+    "/items/:id",
+    validateToken, 
+    validateSchema(updateMenuItemSchema), 
+    authorizedRoles("Developer", "ADMIN"),
+    MenuController.updateMenuItemController
+);
+router.delete(
+    "/items/:id", 
+    validateToken,
+    authorizedRoles("Developer", "ADMIN"),
+    MenuController.deleteMenuItemController
+);
 
 export default router;
