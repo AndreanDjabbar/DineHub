3135f944cab4d11c41e1fc2f256064c89eb39d12
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-01-28 17:09:21 +0700

    Add: create user rate limiter and global rate limiter

---
+++ server/src/middleware/limiter.middleware.js
@@ -0,0 +1,54 @@
+import { rateLimit } from 'express-rate-limit';
+import { RedisStore } from 'rate-limit-redis';
+import { getRedisClient } from '../config/redis.config.js';
+import { GLOBAL_RATE_LIMITER_REQ, GLOBAL_RATE_LIMITER_TIME } from '../util/env.util.js';
+import { responseError } from '../util/response.util.js';
+
+export const globalLimiter = rateLimit({
+    windowMs: GLOBAL_RATE_LIMITER_TIME*60*1000,
+    max: GLOBAL_RATE_LIMITER_REQ,
+    standardHeaders: true, 
+    legacyHeaders: false,
+    store: new RedisStore({
+        sendCommand: async (...args) => {
+            const client = await getRedisClient();
+            return client.call(...args);
+        },
+        prefix: 'limit:global:',
+    }),
+    handler: (req, res) => {
+        return responseError(
+            res, 
+            429,
+            'You have reached your request limit. Please try again later.', 
+            'rateLimitInfo',
+        );
+    },
+});
+
+export const userLimiter = (minuteTime, requestAmount, prefix = 'user') => {
+    return rateLimit({
+        windowMs: Number(minuteTime) * 60 * 1000,
+        max: requestAmount,
+        standardHeaders: true,
+        legacyHeaders: false,
+        keyGenerator: (req) => {
+            return req.user?.id || req.ip; 
+        },
+        store: new RedisStore({
+            sendCommand: async (...args) => {
+                const client = await getRedisClient();
+                return client.call(...args);
+            },
+            prefix: `limit:${prefix}:`,
+        }),
+        handler: (req, res) => {
+            return responseError(
+                res, 
+                429,
+                'You have reached your request limit. Please try again later.', 
+                'rateLimitInfo',
+            );
+        },
+    });
+};
