d7217aaebaf9be429dd1f315c74231e8f9bbe181
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-01-28 14:23:49 +0700

    Add: use helmet library for app

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import helmet from "helmet";
 import logger from "../logs/logger.js";
 import prisma from "./config/postgres.config.js";
 import { getRedisClient } from "./config/redis.config.js";
@@ -18,6 +19,19 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(helmet())
+// When a browser (the user) talks to your server (the API), they exchange a lot of "metadata" called HTTP Headers. By default, Express is very "talkative" and leaves certain security doors wide open. Helmet automatically tightens those headers to make your site much harder to attack.
+/*
+1. It hides your identity (The "Spy" defense)
+By default, Express tells the whole world: "Hey! I am an Express server!" Helmet stops this. If a hacker doesn't know what engine your car has, they don't know which parts to sabotage.
+
+2. It stops "Ghost Clicks" (The "Invisibility" defense)
+It prevents other malicious websites from putting your website inside a tiny invisible window (an iframe) on their own page. This stops hackers from tricking your users into clicking "Delete Account" when they think they are clicking "Play Video."
+
+3. It enforces "Strict Rules" (The "Police" defense)
+It tells the browser: "Only talk to me over secure HTTPS" and "Don't try to guess what kind of file I'm sending you—just trust what I say." This prevents hackers from sneaking malicious code into things like image uploads or CSS files.
+*/
 
 // Increase payload size limit to handle base64 images
 app.use(express.json({ limit: "50mb" }));
