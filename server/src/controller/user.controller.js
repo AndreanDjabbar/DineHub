a54b45f7a23349dcd47bfcd133e8fa6b20529d02
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-02-02 17:08:46 +0700

    Add: add get my user data controller

--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -18,6 +18,25 @@ export const getProfileController = async (req, res) => {
     );
 };
 
+export const getMyUserDataController = async (req, res) => {  
+    const userID = req.user.userID; 
+    if(!userID) {
+        const error = new Error("User ID not found in token");
+        error.statusCode = 400;
+        throw error;
+    }
+    const result = await UserService.getMyUserData(userID);
+    return responseSuccess(
+        res,
+        200,
+        "My user data retrieved successfully",
+        "data",
+        {
+            user: result
+        }
+    );
+};
+
 export const createTenantController = async (req, res) => {
     const result = await UserService.createTenant(req.body);
     return responseSuccess(res, 201, "Tenant created successfully", "data", result);
