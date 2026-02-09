7bdb56917a4b8e405f7b4c42116092e837f84b95
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-02-09 16:18:17 +0700

    Add: add get started button

--- client/app/pages/home/index.tsx
+++ client/app/pages/home/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Button } from '~/components'
 
 const Home = () => {
     return (
         <div className="min-h-screen bg-gray-50 font-sans text-gray-900 flex items-center justify-center">
-            <div className="text-center">
+            <div className="text-center flex flex-col gap-6">
                 <h1 className="text-3xl font-bold text-gray-900 mb-2">Welcome to DineHub</h1>
+                <Button onClick={() => window.location.href = '/menu'}>Get Started</Button>
                 <p className="text-gray-500">Your restaurant ordering system</p>
             </div>
         </div>
