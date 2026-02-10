c6464514eff9363c6742315df0dd3be6b095837e
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-02-10 20:01:39 +0700

    Add: add public routes checking

--- client/app/lib/axios.ts
+++ client/app/lib/axios.ts
@@ -5,13 +5,35 @@ const api = axios.create({
     headers: {
         'Content-Type': 'application/json',
     },
+    withCredentials: true,
 });
 
 api.interceptors.response.use(
     (response) => response,
     (error) => {
         if (error?.response?.status === 401) {
-            if (typeof window !== 'undefined') {
+            const pathName = window?.location?.pathname || '/';
+            
+            const publicRoutes = [
+                '/menu',
+                '/cart',
+                '/orders',
+                '/login',
+                '/signup',
+                '/forgot-password',
+                '/reset-password',
+                '/verify-otp',
+                '/register-tenant',
+                '/partner',
+                '/partner-payment',
+                '/'
+            ];
+            
+            const isPublicRoute = publicRoutes.some(route => 
+                pathName === route || pathName.startsWith(`${route}?`) || pathName.startsWith(`${route}/`)
+            );
+            
+            if (!isPublicRoute) {
                 window.location.href = '/login';
             }
         }
