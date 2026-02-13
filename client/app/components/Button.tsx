47c77742d1283fb4cdfaf0c2535488976e14e255
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-02-13 20:41:11 +0700

    Add: add additional props

--- client/app/components/Button.tsx
+++ client/app/components/Button.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { BiLoader } from "react-icons/bi";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
   className?: string;
+  text?: string;
+  isLoading?: boolean;
+  isLoadingText?: string;
 }
 
 const Button: React.FC<ButtonProps> = ({
-  children,
   className = "",
   ...props
 }) => {
@@ -15,7 +17,14 @@ const Button: React.FC<ButtonProps> = ({
     className={`hover:cursor-pointer w-full bg-red-600 text-white px-6 py-3 rounded-lg font-bold hover:bg-red-700 transition ${className}`}
     {...props}
     >
-      {children}
+      {props.isLoading ? (
+          <div className="text-bold flex items-center justify-center gap-2">
+            <BiLoader size={20} className="animate-spin" />
+            {props.isLoadingText || "Loading..."}
+          </div>
+        ) : (
+          props.text || "Submit"
+        )}
     </button>
   );
 };
