2803320ad1c54bb0aba90fda9cabd5013ae010af
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-02-14 16:17:00 +0700

    Update: update register request using use request hooks

--- client/app/pages/account/signup.tsx
+++ client/app/pages/account/signup.tsx
@@ -1,19 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, NavLink } from "react-router";
 import {
-  FiArrowLeft,
   FiMail,
   FiLock,
-  FiEye,
-  FiEyeOff,
   FiUser,
 } from "react-icons/fi";
-import { BottomNavigation, TextInput, BackButton, Button } from "~/components";
-import api from "~/lib/axios";
+import { BottomNavigation, TextInput, Button } from "~/components";
+import { useRequest } from "~/hooks";
 
 const Signup: React.FC = () => {
   const navigate = useNavigate();
-
+  const { 
+    makeRequest: registerRequest,
+    isSuccess,
+    validationErrors: registrationValidationErrors,
+    data: registerData,
+    isLoading: registerLoading,
+  } = useRequest();
+  
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -21,50 +25,34 @@ const Signup: React.FC = () => {
     confirmPassword: "",
   });
 
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  const [showPassword, setShowPassword] = useState(false);
-  const [showConfirm, setShowConfirm] = useState(false);
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    setError("");
 
-    if (formData.password !== formData.confirmPassword) {
-      setError("Passwords do not match");
-      return;
-    }
-
-    setIsLoading(true);
-
-    try {
-      const response = await api.post(
-        "http://localhost:4000/dinehub/api/auth/register",
-        {
-          name: formData.name,
-          email: formData.email,
-          password: formData.password,
-        }
-      );
-
-      const data = response.data;
-      const token = data.data?.token;
-        setIsLoading(false);
-        navigate(`/verify-otp?email=${formData.email}&token=${token}`);
-    } catch (err: any) {
-      setError(err?.data.message || "Signup failed");
-    } finally {
-      setIsLoading(false);
-    }
+    await registerRequest({
+      method: "POST",
+      url: "http://localhost:4000/dinehub/api/auth/register",
+      payload: {
+        name: formData.name,
+        email: formData.email,
+        password: formData.password,
+        confirmPassword: formData.confirmPassword,
+      }
+    });
   };
 
+  useEffect(() => {
+    if (isSuccess) {
+      const token = registerData?.data?.token;
+      navigate(`/verify-otp?email=${formData.email}&token=${token}`);
+    }
+  }, [isSuccess, formData.email, navigate]);
+
   return (
-    <div className="min-h-screen bg-gray-50 font-sans text-gray-900 px-6 py-6 flex flex-col">
+    <div className="min-h-screen bg-gray-50 font-sans text-gray-900 px-6 py-6 flex flex-col pb-20">
       <div className="mb-8">
         <h1 className="text-3xl font-bold mb-2 text-gray-900">
           Create Account
@@ -72,116 +60,105 @@ const Signup: React.FC = () => {
         <p className="text-gray-500">Sign up to get started!</p>
       </div>
 
-      <form className="flex-col flex gap-5" onSubmit={handleSubmit}>
-        {/* Name Field */}
-        <div className="space-y-2">
-          <label className="text-sm font-bold text-gray-700 ml-1">
-            Full Name
-          </label>
-          <div className="relative">
-            <TextInput
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              placeholder="Your Name"
-              required
-              icon={FiUser}
-            />
+      <div className="">
+        <form className="flex-col flex gap-5" onSubmit={handleSubmit}>
+          {/* Name Field */}
+          <div className="space-y-2">
+            <label className="text-sm font-bold text-gray-700 ml-1">
+              Full Name
+            </label>
+            <div className="relative">
+              <TextInput
+                type="text"
+                name="name"
+                error={registrationValidationErrors?.name}
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="Your Name"
+                required
+                icon={FiUser}
+              />
+            </div>
           </div>
-        </div>
 
-        {/* Email Field */}
-        <div className="space-y-2">
-          <label className="text-sm font-bold text-gray-700 ml-1">Email</label>
-          <div className="relative">
-            <TextInput
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="user@example.com"
-              required
-              icon={FiMail}
-            />
+          {/* Email Field */}
+          <div className="space-y-2">
+            <label className="text-sm font-bold text-gray-700 ml-1">Email</label>
+            <div className="relative">
+              <TextInput
+                type="email"
+                name="email"
+                value={formData.email}
+                error={registrationValidationErrors?.email}
+                onChange={handleChange}
+                placeholder="user@example.com"
+                required
+                icon={FiMail}
+              />
+            </div>
           </div>
-        </div>
 
-        {/* Password Field */}
-        <div className="space-y-2">
-          <label className="text-sm font-bold text-gray-700 ml-1">
-            Password
-          </label>
-          <div className="relative">
-            <TextInput
-              type={showPassword ? "text" : "password"}
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              placeholder="••••••••"
-              required
-              icon={FiLock}
-            />
-            <button
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600 p-1"
+          {/* Password Field */}
+          <div className="space-y-2">
+            <label className="text-sm font-bold text-gray-700 ml-1">
+              Password
+            </label>
+            <div className="relative">
+              <TextInput
+                type={"password"}
+                name="password"
+                error={registrationValidationErrors?.password}
+                value={formData.password}
+                onChange={handleChange}
+                placeholder="••••••••"
+                required
+                icon={FiLock}
+              />
+            </div>
+          </div>
+
+          {/* Confirm Password Field */}
+          <div className="space-y-2">
+            <label className="text-sm font-bold text-gray-700 ml-1">
+              Confirm Password
+            </label>
+            <div className="relative">
+              <TextInput
+                type={"password"}
+                error={registrationValidationErrors?.confirmPassword}
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                placeholder="••••••••"
+                required
+                icon={FiLock}
+              />
+            </div>
+          </div>
+
+          <Button 
+          type="submit" 
+          isLoading={registerLoading} 
+          isLoadingText="Signing Up..."
+          text="Sign Up" />
+        </form>
+
+        {/* --- Footer: Login Link --- */}
+        <div className="grow flex justify-center pb-4 mt-6">
+          <p className="text-sm font-medium text-gray-500">
+            Already have an account?{" "}
+            <NavLink
+              to="/login"
+              className="text-red-600 font-bold hover:underline"
             >
-              {showPassword ? (
-                <FiEyeOff className="w-5 h-5" />
-              ) : (
-                <FiEye className="w-5 h-5" />
-              )}
-            </button>
-          </div>
+              Login
+            </NavLink>
+          </p>
         </div>
-
-        {/* Confirm Password Field */}
-        <div className="space-y-2">
-          <label className="text-sm font-bold text-gray-700 ml-1">
-            Confirm Password
-          </label>
-          <div className="relative">
-            <TextInput
-              type={showConfirm ? "text" : "password"}
-              name="confirmPassword"
-              value={formData.confirmPassword}
-              onChange={handleChange}
-              placeholder="••••••••"
-              required
-              icon={FiLock}
-            />
-            <button
-              type="button"
-              onClick={() => setShowConfirm(!showConfirm)}
-              className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600 p-1"
-            >
-              {showConfirm ? (
-                <FiEyeOff className="w-5 h-5" />
-              ) : (
-                <FiEye className="w-5 h-5" />
-              )}
-            </button>
-          </div>
-        </div>
-
-        {/* Submit Button */}
-        <Button type="submit">Sign Up</Button>
-      </form>
-
-      {/* --- Footer: Login Link --- */}
-      <div className="grow flex justify-center pb-4 mt-6">
-        <p className="text-sm font-medium text-gray-500">
-          Already have an account?{" "}
-          <NavLink
-            to="/login"
-            className="text-red-600 font-bold hover:underline"
-          >
-            Login
-          </NavLink>
-        </p>
       </div>
+
       <BottomNavigation />
+
     </div>
   );
 };
